dc3cd6b79f2f5e8731aafd6dfcc4061d395ef302
Ubuntu <ubuntu@ip-172-31-14-97.ap-northeast-1.compute.internal>
2022-08-06 04:48:10 +0000

    memberを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_04_233058) do
-
-  create_table "members", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2022_08_05_224348) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.integer "score"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
