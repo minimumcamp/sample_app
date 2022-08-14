8b2f2dc9b9a3fba6184a1c1dfcc25f83a2eb8219
Ubuntu <ubuntu@ip-172-31-14-97.ap-northeast-1.compute.internal>
2022-08-14 01:14:04 +0000

    ３つのエラーを残したまま一度完了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,4 @@
 module SessionsHelper
-
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
@@ -16,14 +15,10 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
-  
+
   # 現在のユーザーをログアウトする
   def log_out
     session.delete(:user_id)
     @current_user = nil
   end
-  
-  def destroy
-    log_out
-    redirect_to root_url
 end
